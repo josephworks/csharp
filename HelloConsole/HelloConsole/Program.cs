15799917dbdbb68ff3f705b767094092c9eaba44
Joseph Robinson <smartech56@gmail.com>
2019-08-12 09:31:53 -0700

    Added HelloConsole app

---
+++ HelloConsole/HelloConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Console!");
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
