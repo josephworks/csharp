9c718d1dffc27ac6289f5d92fe4f3b1d147d1707
Joseph Robinson <smartech56@gmail.com>
2019-08-13 09:33:10 -0700

    updated Hello world app and added dialog

--- HelloConsole/HelloConsole/Program.cs
+++ HelloConsole/HelloConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console; //Allows you tu use "Console." without typing "Console."
 
 namespace HelloConsole
 {
@@ -10,8 +11,19 @@ namespace HelloConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Console!");
+            string first = "";
+            string last = "";
+            string age = "";
             Console.Beep();
+            Console.WriteLine("Console > Hello World!");
+            Console.WriteLine("Console > What is your First name?");
+            first = ReadLine();
+            Console.WriteLine("Console > What is your Last name?");
+            last = ReadLine();
+            Console.WriteLine("Console > Nice to finally meet you " + last + ", " + first);
+            Console.WriteLine("Console > Oh, I forgot to ask, how old are you?");
+            age = ReadLine();
+            Console.WriteLine("Console > Got it, you are " + age + " years old");
             Console.ReadLine();
         }
     }
