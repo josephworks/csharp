e2f3bf70f78929a6596cb7f43b3d2fbc9b8c846d
Joseph Robinson <smartech56@gmail.com>
2019-08-15 09:30:44 -0700

    Added required ascii art for school

--- ASCIIText/ASCIIText/Program.cs
+++ ASCIIText/ASCIIText/Program.cs
@@ -16,6 +16,38 @@ namespace ASCIIText
             Console.WriteLine(
                 FiggleFonts.Standard.Render("Hello, World!"));
             Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(@"    J RRRR  
+    J R   R 
+    J RRRR  
+J   J R R   
+ JJJ  R  RR ");
+            Console.WriteLine(@"
+
+     ,/|\
+     //&')
+     '')(
+      (( )
+      )( (
+      (=M=[)###########>
+      (( )
+      (( )_
+      ((__,)");
+            Console.WriteLine(@"+------------------+             XXXXXXXXXXXXX                     XX
+|                  |           XXX            XX                   X XX
+|                  |           X                X                 XX  XX
+|                  |          X                 X                XX    XX
+|                  |          X                 X                X      XX
+|                  |          XX                X               X        XXX
+|                  |           XX              XX              XX          XX
+|                  |            X             XX               X            XXX
+|                  |             XX         XX                X               XX
+|                  |              XX      XXX                XX                XX
++------------------+               XXX XXXX                 XX                   XX
+                                                            XXXXXXXXXXXXXXXXXXXXXXX
+
+");
+            Console.ReadLine();
         }
     }
 }
