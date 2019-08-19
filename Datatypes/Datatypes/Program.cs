17ec36b9ad44192da2dcaad253f759d18cbc4bd4
Joseph Robinson <smartech56@gmail.com>
2019-08-19 09:32:55 -0700

    Added datatypes project

---
+++ Datatypes/Datatypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.WriteLine("Maxmum Value: " + int.MaxValue);
+            Console.WriteLine("Minimum Value: " + int.MinValue);
+            Console.ReadLine();
+        }
+    }
+}
