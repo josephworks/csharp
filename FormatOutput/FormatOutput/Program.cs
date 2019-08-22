439c69808c9a4b5fee493c73aa07269be4390aa5
Joseph Robinson <smartech56@gmail.com>
2019-08-22 09:20:55 -0700

    Updated FormatOutput

--- FormatOutput/FormatOutput/Program.cs
+++ FormatOutput/FormatOutput/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace FormatOutput
 {
@@ -10,6 +11,17 @@ namespace FormatOutput
     {
         static void Main(string[] args)
         {
+            Beep(); //Honestly, This is VERRY IMPORTNT
+            const double miles = 4.5;
+            WriteLine(miles);
+            ReadLine();
+            WriteLine("{0:N2}", miles);
+            ReadLine();
+            WriteLine("Converting to Feet and Inches...");
+            WriteLine("Feet: " + miles * 5280);
+            WriteLine("Inches: " + miles * 63360);
+            ReadLine();
+
         }
     }
 }
