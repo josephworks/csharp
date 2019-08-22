254a226d00e4ca6834d4f0e0f944957165923822
Joseph Robinson <smartech56@gmail.com>
2019-08-22 08:33:27 -0700

    shortened vars

--- Datatypes/Datatypes/Program.cs
+++ Datatypes/Datatypes/Program.cs
@@ -11,20 +11,20 @@ namespace Datatypes
     {
         static void Main(string[] args)
         {
-            Console.Beep();
+            Console.Beep(;
             Console.WriteLine("Maxmum Value: " + int.MaxValue);
             Console.WriteLine("Minimum Value: " + int.MinValue);
             Console.ReadLine();
             byte x = 1;
             sbyte y = 2;
             char z = 3;
-            decimal d = 5.23;
+            decimal d =9.954758945;
             double b = 22;
             float f = 33;
             int i = 123;
             uint u = 2334;
             long l = 123456789;
-            ulong e = 123456789123;
+            ulong e = 12345678;
             short s = 255;
             ushort h = 12;
             WriteLine(x);
@@ -39,9 +39,8 @@ namespace Datatypes
             WriteLine(e);
             WriteLine(s);
             WriteLine(h);
+            WriteLine("My sallary is: " + "{0:c}"+d,f,1);
             ReadLine();
-            WriteLine("My sallary is: "+"{0:c}",26666.667);
-           ReadLine();
         }
     }
 }
