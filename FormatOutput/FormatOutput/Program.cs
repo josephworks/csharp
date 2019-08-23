6a74d098c946b04883756b874879c40bf54a9e5d
Joseph Robinson <smartech56@gmail.com>
2019-08-23 08:59:42 -0700

    Finished FormatConsole

--- FormatOutput/FormatOutput/Program.cs
+++ FormatOutput/FormatOutput/Program.cs
@@ -11,17 +11,35 @@ namespace FormatOutput
     {
         static void Main(string[] args)
         {
-            Beep(); //Honestly, This is VERRY IMPORTNT
-            const double miles = 4.5;
-            WriteLine(miles);
+            ForegroundColor = ConsoleColor.Green;
+            Beep();
+            double miles = 4.5;
+            double feet;
+            double inches;
+            feet = 5280 * miles;
+            inches = 12 * feet;
+            WriteLine("Miles: {0:N2}", miles);
+            WriteLine("Feet: {0}", feet);
+            WriteLine("Inches: {0}", inches);
+            WriteLine("");
+            miles = 3.5;
+            double km;
+            km = miles * 1.60934;
+            WriteLine("Miles: {0:N2}", miles);
+            WriteLine("Kilometers: {0:N2}", km);
+            WriteLine("");
+            double c = 0;
+            double f;
+            f = (c * (9 / 5)) + 32;
+            WriteLine("Celsius: {0:N2}", c);
+            WriteLine("Fahrenheit: {0:N2}", f);
+            WriteLine("");
+            double r = 52;
+            double m;
+            m = 52 * 1.15;
+            WriteLine("Price: {0:C2}", r);
+            WriteLine("Price after Markup: {0:C2}", m);
             ReadLine();
-            WriteLine("{0:N2}", miles);
-            ReadLine();
-            WriteLine("Converting to Feet and Inches...");
-            WriteLine("Feet: " + miles * 5280);
-            WriteLine("Inches: " + miles * 63360);
-            ReadLine();
-            
         }
     }
 }
