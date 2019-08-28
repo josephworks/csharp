58c9b0e9b72f450583d24726f5d331f8d2e56161
Joseph Robinson <smartech56@gmail.com>
2019-08-28 09:21:03 -0700

    Added numbers processing

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string aValue;
+            Write("Enter number of feet:");
+            aValue = ReadLine();
+            num = int.Parse(aValue);
+
+        }
+    }
+}
