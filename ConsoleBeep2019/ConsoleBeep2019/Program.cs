dc511ebdaf0eeb0fed44edc70fcd2ec2527a796c
Joseph Robinson <smartech56@gmail.com>
2019-08-28 08:33:01 -0700

    Added console.beep2019

---
+++ ConsoleBeep2019/ConsoleBeep2019/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBeep2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
