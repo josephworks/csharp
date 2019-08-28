06206b02553d750dc587adfffd5df60a525006ec
Joseph Robinson <smartech56@gmail.com>
2019-08-28 08:38:37 -0700

    Added static

---
+++ static/static/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace @static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadLine();
+        }
+
+        private int Sqrd(int num)
+        {
+            return num * num;
+        }
+    }
+}
