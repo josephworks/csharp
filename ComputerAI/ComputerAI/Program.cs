f156bccf24a2beaa6ff281d9f40c1c94103b66f9
Joseph Robinson <smartech56@gmail.com>
2019-08-28 08:51:34 -0700

    Uploaded UNSTABLE DialogFlow Console App

---
+++ ComputerAI/ComputerAI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Dialogflow.V2;
+using Google.Apis.Discovery.v1;
+using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Services;
+using static System.Console;
+
+namespace ComputerAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Google.Cloud.Dialogflow.V2.Agent;
+    }
+}
