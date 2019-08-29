5b0f97c7efe2fc78c28a050eb20ad71c6d02241a
Joseph Robinson <smartech56@gmail.com>
2019-08-29 09:44:36 -0700

    Reset Code for new assignment

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -11,12 +11,30 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
-            int num;
-            string aValue;
-            Write("Enter number of feet:");
-            aValue = ReadLine();
-            num = int.Parse(aValue);
+            //int num;
+            //string aValue;
+            //Write("Enter number of feet:");
+            //aValue = ReadLine();
+            //num = int.Parse(aValue);
+            //WriteLine(num)
 
+            String name;
+            WriteLine("What is your name?");
+            name = ReadLine();
+            WriteLine("Your Name is {0}", name);
+            ReadKey();
+
+            String school;
+            WriteLine("What school do you go to?");
+            school = ReadLine();
+            WriteLine("Your go to {0}", school);
+            ReadKey();
+            
+            String saying;
+            WriteLine("What is your favorite saying?");
+            saying = ReadLine();
+            WriteLine("Your favorite saying is: {0}", saying);
+            ReadKey();
         }
     }
 }
