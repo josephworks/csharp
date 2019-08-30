4b56203214cd494cea8deba0ea2dc16d4f0f7a52
Joseph Robinson <smartech56@gmail.com>
2019-08-30 08:16:07 -0700

    updated LiveShare url

--- LiveShare.md
+++ LiveShare.md
@@ -2,6 +2,6 @@
 
 ## Use the following URL in order to code with Joseph
 
-https://prod.liveshare.vsengsaas.visualstudio.com/join?2E4A61BFB1336C1EBD37EB03D6698CBE40CB
+https://prod.liveshare.vsengsaas.visualstudio.com/join?CF48E96273D6BC1EA473BEF0685175F6C14E
 
 Lets Establish Communism (:
--- RunPython/RunPython/Program.cs
+++ RunPython/RunPython/Program.cs
@@ -14,7 +14,7 @@ namespace Numbers
         {
             Microsoft.Scripting.Hosting.ScriptEngine pythonEngine =
                 IronPython.Hosting.Python.CreateEngine();
-            // We execute this script from Visual Studio 
+            // We execute this script from Visual Studio
             // so the program will be executed from bin\Debug or bin\Release
             Microsoft.Scripting.Hosting.ScriptSource pythonScript =
                 pythonEngine.CreateScriptSourceFromFile("..\\..\\python.py");
