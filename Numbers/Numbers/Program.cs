b1f01bde7eaf16a6b1f4b25a7458f2e75c6f5d20
Joseph Robinson <smartech56@gmail.com>
2019-09-03 09:08:49 -0700

    updated numbers

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -11,30 +11,44 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
+            Console.Beep(); //Most important
+
             //int num;
             //string aValue;
             //Write("Enter number of feet:");
             //aValue = ReadLine();
             //num = int.Parse(aValue);
-            //WriteLine(num)
+            //WriteLine();
+
+            //Todo > Convert to methods
+
+            ForegroundColor = ConsoleColor.Green;
 
             String name;
             WriteLine("What is your name?");
             name = ReadLine();
             WriteLine("Your Name is {0}", name);
-            ReadKey();
+            WriteLine("");
 
-            String school;
-            WriteLine("What school do you go to?");
-            school = ReadLine();
-            WriteLine("Your go to {0}", school);
-            ReadKey();
-            
             String saying;
             WriteLine("What is your favorite saying?");
             saying = ReadLine();
             WriteLine("Your favorite saying is: {0}", saying);
             ReadKey();
+
+            String school;
+            WriteLine("What school do you go to?");
+            school = ReadLine();
+            WriteLine("You go to {0}", school);
+            ReadKey();
+
+            String output;
+            WriteLine("{0} - {1} {2}", saying, name, school);
+        }
+
+        public static string student(string name)
+        {
+            return name;
         }
     }
 }
