ca5acba51fc58b1710cc8d23dacde351b623fd43
Joseph Robinson <smartech56@gmail.com>
2019-09-05 09:10:31 -0700

    Added second methodspractice

---
+++ MethodPractice2/MethodPractice2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MethodPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printout(“Richard”, “Bob”, “Jones”);
+        }
+        public static string printout()
+        {
+
+        }
+    }
+}
