65ab3adb33ceb8af8f8012902c918ec7d4cafdbc
Joseph Robinson <smartech56@gmail.com>
2019-09-10 10:41:24 -0700

    Remove blank lines

--- AdvAreaCalc/Program.cs
+++ AdvAreaCalc/Program.cs
@@ -4,43 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-namespace Userinput
+namespace AdvAreaCalc
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             WriteLine("The area of a circle - 1, the area of a rectangle - 2, the area of a trapezoid - 3, or the surface area of a cylinder - 4");
-
             int input = int.Parse(ReadLine());
-
             switch (input)
             {
                 case 1:
                     WriteLine("Circle");
-
                     WriteLine("What is the radius?");
                     double radius = double.Parse(ReadLine());
-
                     double circle = (Math.PI * (radius * radius));
-
                     WriteLine(circle);
-
                     ReadLine();
-
                     break;
                 case 2:
                     WriteLine("Rectangle");
-
                     WriteLine("What is the length?");
                     double rectanglearea = double.Parse(ReadLine());
-
                     WriteLine("What is the width?");
                     double rectanglelength = double.Parse(ReadLine());
-
                     double rectangle = (rectanglearea * rectanglelength);
-
                     WriteLine(rectangle);
                     ReadLine();
                     break;
