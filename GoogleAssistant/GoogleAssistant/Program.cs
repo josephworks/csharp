8876ebb74baa01992c815872c4ceb8a4e4832ea7
Joseph Robinson <smartech56@gmail.com>
2019-09-12 09:52:31 -0700

    Added GoogleAssistant Project

---
+++ GoogleAssistant/GoogleAssistant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using IronPython;
+using static System.Console;
+
+namespace GoogleAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Scripting.Hosting.ScriptEngine pythonEngine =
+                IronPython.Hosting.Python.CreateEngine();
+            // We execute this script from Visual Studio
+            // so the program will be executed from bin\Debug or bin\Release
+            Microsoft.Scripting.Hosting.ScriptSource pythonScript =
+                pythonEngine.CreateScriptSourceFromFile("..\\..\\python.py");
+            Console.Beep(); //Plays Beep before running python
+            pythonScript.Execute();
+            ReadLine();
+        }
+    }
+}
