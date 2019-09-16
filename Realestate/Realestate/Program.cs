2c40184349abae8d15dd31a0ec147a426ee8fa03
Joseph Robinson <smartech56@gmail.com>
2019-09-16 09:33:11 -0700

    Started Realestate Project

---
+++ Realestate/Realestate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Realestate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beep();
+        }
+    }
+}
---
+++ Realestate/Realestate/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realestate
+{
+    class Property
+    {
+        int m_year;
+        int m_price;
+        int m_income;
+        int m_expense;
+        string m_address;
+        public int Earnings(int income, int expense)
+        {
+            return
+        }
+    }
+}
