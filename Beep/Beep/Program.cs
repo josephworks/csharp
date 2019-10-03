78eda6cd8f28887654751c32b2d7e6c31016923c
Joseph Robinson <smartech56@gmail.com>
2019-10-02 17:43:03 -0700

    added imports for AudioSwitcher

--- Beep/Beep/Program.cs
+++ Beep/Beep/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AudioSwitcher;
+using AudioSwitcher.AudioApi;
 using AudioSwitcher.AudioApi.CoreAudio;
 using static System.Console;
 
