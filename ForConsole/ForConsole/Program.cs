e6d51bf335d03c7872d83c86fef5744abc6c8cf1
Joseph Robinson <smartech56@gmail.com>
2019-10-17 09:06:09 -0700

    Added ForConsole

---
+++ ForConsole/ForConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ForConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beep(1000, 100);
+            int num = 0;
+            WriteLine("Enter a number");
+            num = int.Parse(ReadLine());
+            for (int r = 1; r <= num; r++)
+            {
+                for (int c = 1; c <= num; c++)
+                {
+                    Write("*" + "\t");
+                }
+                WriteLine();
+            }
+            ReadLine();
+        }
+    }
+}
