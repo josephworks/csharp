92d3b2a8023390079c2443fbdf12bc93ce1df014
Joseph Robinson <smartech56@gmail.com>
2019-10-17 09:06:23 -0700

    added forloops examples

---
+++ forloops/forloops/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace forloops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Beep();
+        }
+    }
+}
