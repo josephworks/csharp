2e8cd0d7d03a0bef04337511c06ca2503598da8a
Joseph Robinson <smartech56@gmail.com>
2019-10-17 09:04:52 -0700

    Added arrays code

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(1000, 100);
+            double[] myDoubles = new double[10];
+            //int size = 0;
+            double sum = 0;
+            //WriteLine("Amount of numbers: ");
+            //size = int.Parse(ReadLine());
+            for (int i = 0; i < myDoubles.Length; i++)
+            {
+                myDoubles[i] = Double.Parse(ReadLine());
+            }
+
+            foreach (double item in myDoubles)
+            {
+                sum = sum + item;
+            }
+            ReadLine();
+            /*Console.Beep();
+            double[] myDoubles = new double[10];
+            int[] myInts = new int[1000000];
+            for (int i = 1; i <= 1000000; i++)
+            {
+                myInts[i - 1] = i;
+            }
+
+            for (int i = 0; i < myInts.Length; i++)
+            {
+                //myInts[i] = 7;
+            }
+
+            foreach (int item in myInts)
+            {
+                WriteLine(item);
+            }
+
+            Console.Beep();
+            ReadLine();*/
+        }
+    }
+}
