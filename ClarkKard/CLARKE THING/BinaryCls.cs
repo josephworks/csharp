1fda1e40d6e237d795b7600218a0dc95e135affa
Joseph Robinson <smartech56@gmail.com>
2019-11-06 09:15:48 -0700

    Added clark card game

---
+++ ClarkKard/CLARKE THING/BinaryCls.cs
@@ -0,0 +1,40 @@
+﻿namespace CLARKE_THING
+{
+    internal class BinaryCls
+    {
+        private int[] m_Data = new int[16];
+
+        public BinaryCls(int num)
+        {
+            int rem = 0;
+            int div = 0;
+            for (int i = 0; i < m_Data.Length; i++)
+            {
+                div = num / 2;
+                rem = num % 2;
+                m_Data[i] = rem;
+                num = div;
+            }
+        }
+
+        public override string ToString()
+        {
+            string retVal = "";
+            for (int i = 0; i < m_Data.Length; i++)
+            {
+                retVal = m_Data[i] + retVal;
+            }
+            return retVal;
+        }
+
+        public string ToOctal()
+        {
+            string retVal = "";
+            for (int i = 0; i < m_Data.Length; i = i + 3)
+            {
+                retVal = m_Data[i] + retVal;
+            }
+            return retVal;
+        }
+    }
+}
---
+++ ClarkKard/CLARKE THING/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace ClarkKard
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BinaryCls Bin = new BinaryCls(10);
+            WriteLine(Bin);
+            ReadLine();
+        }
+    }
+}
