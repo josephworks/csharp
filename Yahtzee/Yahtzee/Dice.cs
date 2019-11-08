37a47cfe3f221de190690a4976dc4bd4253dc6c7
Joseph Robinson <smartech56@gmail.com>
2019-11-08 08:59:17 -0700

    Added Yahzee

---
+++ Yahtzee/Yahtzee/Dice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class Dice
+    {
+        private int m_value;
+        public void Roll()
+        {
+            Random r = new Random();
+            m_value = r.Next(1, 7);
+        }
+
+        public int getValue()
+        {
+            return m_value;
+        }
+    }
+}
---
+++ Yahtzee/Yahtzee/SetOfDice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Yahtzee
+{
+    class SetOfDice
+    {
+        Dice[] m_dice = new Dice[5];
+        //Put shoes in box
+        public SetOfDice()
+        {
+            for (int i = 0; i < m_dice.Length; i++)
+            {
+                m_dice[i] = new Dice();
+            }
+        }
+        public void Roll()
+        {
+            foreach (Dice d in m_dice)
+            {
+                d.Roll();
+            }
+        }
+
+        public Boolean Hasthreeofakind()
+        {
+            int[] vals = new int[5];
+            for (int i = 0; i < vals.Length; i++)
+            {
+                vals[i] = m_dice[i].getValue();
+            }
+            Array.Sort(vals);
+            for (int i = 0; i < 3; i++)
+            {
+                if (vals[i] == vals[i+1] &&
+                    vals[i+1] == vals[i+2])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
