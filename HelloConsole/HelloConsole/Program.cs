ec277d6f12d0cbebee9a21ee0aee7fff2bec68ab
Joseph Robinson <smartech56@gmail.com>
2019-11-19 15:48:05 -0700

    Updated HelloConsole

--- HelloConsole/HelloConsole/Program.cs
+++ HelloConsole/HelloConsole/Program.cs
@@ -11,9 +11,14 @@ namespace HelloConsole
     {
         static void Main(string[] args)
         {
+            //TODO:  Street, Zip, Phone, Email
             string first = "";
             string last = "";
             string age = "";
+            string street = "";
+            string zip = "";
+            string phone = "";
+            string email = "";
             Console.ForegroundColor = ConsoleColor.Green;
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Beep();
@@ -23,10 +28,30 @@ namespace HelloConsole
             first = ReadLine();
             Console.WriteLine("Console > What is your Last name?");
             last = ReadLine();
-            Console.WriteLine("Console > Nice to finally meet you " + last + ", " + first);
+            Console.WriteLine("Console > Nice to finally meet you " + last + ", " + first + ".");
             Console.WriteLine("Console > Oh, I forgot to ask, how old are you?");
             age = ReadLine();
-            Console.WriteLine("Console > Got it, you are " + age + " years old");
+            Console.WriteLine("Console > Got it, you are " + age + " years old.");
+            Console.WriteLine("Console > What is your street address?");
+            street = ReadLine();
+            Console.WriteLine("Console > Got it, you are living on / near " + street + ".");
+            Console.WriteLine("Console > What is your zip code?");
+            zip = ReadLine();
+            Console.WriteLine("Console > Got it, you are located in / near " + zip + ".");
+            Console.WriteLine("Console > What is your phone number?");
+            phone = ReadLine();
+            Console.WriteLine("Console > Got it, your phone number is " + phone + ".");
+            Console.WriteLine("Console > Last question, What is your email?");
+            email = ReadLine();
+            Console.WriteLine("Console > Got it, your email is " + email + ".");
+            Console.WriteLine("Thanks for the information!");
+            Console.WriteLine("Information:");
+            Console.WriteLine("Name: " + last + ", " + first);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Street: "  + street);
+            Console.WriteLine("ZIP: " + zip);
+            Console.WriteLine("Phone Number: " + phone);
+            Console.WriteLine("Email: " + email);
             Console.ReadLine();
         }
     }
