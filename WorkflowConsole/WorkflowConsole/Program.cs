69b2197e7ed0792c6936d49f8cf7381f13f6e006
Joseph Robinson <smartech56@gmail.com>
2019-11-21 16:38:21 -0700

    Added WorkflowConsole

---
+++ WorkflowConsole/WorkflowConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using static System.Console;
+
+namespace WorkflowConsole
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beep();
+            Activity workflow1 = new Workflow1();
+            WorkflowInvoker.Invoke(workflow1);
+        }
+    }
+}
