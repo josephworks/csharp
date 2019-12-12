8799743c2dd86a7728fe293d661016dee733590d
Joseph Robinson <smartech56@gmail.com>
2019-12-12 09:03:38 -0700

    Added sudoku stuff

---
+++ SudokuSolver/SudokuSolver/UI/MainWindow.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Kermalis.SudokuSolver.UI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.sudokuBoard = new Kermalis.SudokuSolver.UI.SudokuBoard();
+            this.logList = new System.Windows.Forms.ListBox();
+            this.solveButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.puzzleLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(35, 53);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.sudokuBoard);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.logList);
+            this.splitContainer1.Size = new System.Drawing.Size(941, 470);
+            this.splitContainer1.SplitterDistance = 470;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // sudokuBoard
+            // 
+            this.sudokuBoard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sudokuBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sudokuBoard.Font = new System.Drawing.Font("Leelawadee", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sudokuBoard.Location = new System.Drawing.Point(0, 0);
+            this.sudokuBoard.Margin = new System.Windows.Forms.Padding(0);
+            this.sudokuBoard.Name = "sudokuBoard";
+            this.sudokuBoard.Size = new System.Drawing.Size(470, 470);
+            this.sudokuBoard.TabIndex = 0;
+            // 
+            // logList
+            // 
+            this.logList.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.logList.Font = new System.Drawing.Font("Meiryo", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logList.FormattingEnabled = true;
+            this.logList.HorizontalScrollbar = true;
+            this.logList.ItemHeight = 17;
+            this.logList.Location = new System.Drawing.Point(0, 7);
+            this.logList.Name = "logList";
+            this.logList.Size = new System.Drawing.Size(470, 463);
+            this.logList.TabIndex = 0;
+            // 
+            // solveButton
+            // 
+            this.solveButton.Enabled = false;
+            this.solveButton.Location = new System.Drawing.Point(448, 27);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(75, 23);
+            this.solveButton.TabIndex = 1;
+            this.solveButton.Text = "Solve";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.SolvePuzzle);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1011, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewPuzzle);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenPuzzle);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SavePuzzle);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.Exit);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.puzzleLabel,
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 552);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1011, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // puzzleLabel
+            // 
+            this.puzzleLabel.Name = "puzzleLabel";
+            this.puzzleLabel.Size = new System.Drawing.Size(68, 17);
+            this.puzzleLabel.Text = "puzzleLabel";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(66, 17);
+            this.statusLabel.Text = "statusLabel";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 574);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.solveButton);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "Sudoku Solver";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private SudokuSolver.UI.SudokuBoard sudokuBoard;
+        private System.Windows.Forms.Button solveButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ListBox logList;
+        private System.Windows.Forms.ToolStripStatusLabel puzzleLabel;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ SudokuSolver/SudokuSolver/UI/MainWindow.cs
@@ -0,0 +1,130 @@
+﻿using Kermalis.SudokuSolver.Core;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Kermalis.SudokuSolver.UI
+{
+    partial class MainWindow : Form
+    {
+        Stopwatch stopwatch;
+        Solver solver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            puzzleLabel.Text = "";
+            statusLabel.Text = "";
+            logList.SelectedIndexChanged += LogList_SelectedIndexChanged;
+            sudokuBoard.CellChanged += (cell) => ChangeState(true, true);
+        }
+
+        void LogList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sudokuBoard.ReDraw(true, logList.SelectedIndex);
+        }
+        void ChangeState(bool solveButtonState, bool saveState)
+        {
+            solveButton.Enabled = solveButtonState;
+            saveAsToolStripMenuItem.Enabled = saveState;
+        }
+
+        void ChangePuzzle(string puzzleName, bool solveButtonState)
+        {
+            ChangeState(solveButtonState, false);
+            puzzleLabel.Text = puzzleName + " Puzzle";
+            statusLabel.Text = "";
+            logList.DataSource = solver.Puzzle.Actions;
+            sudokuBoard.SetBoard(solver.Puzzle);
+        }
+        void NewPuzzle(object sender, EventArgs e)
+        {
+            solver = new Solver(new Puzzle(Utils.CreateJaggedArray<int[][]>(9, 9), true));
+            ChangePuzzle("Custom", false);
+            solver.Puzzle.LogAction("Custom puzzle created");
+            MessageBox.Show("A custom puzzle has been created. Click cells to type in values.", Text);
+        }
+        void OpenPuzzle(object sender, EventArgs e)
+        {
+            var d = new OpenFileDialog
+            {
+                Title = "Open Sudoku Puzzle",
+                Filter = "TXT files|*.txt",
+                InitialDirectory = Path.GetFullPath(Directory.GetCurrentDirectory() + "\\..\\Puzzles")
+            };
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                Puzzle puzzle;
+                try
+                {
+                    puzzle = Puzzle.Load(d.FileName);
+                }
+                catch (InvalidDataException)
+                {
+                    MessageBox.Show("Invalid puzzle data.");
+                    return;
+                }
+                catch
+                {
+                    MessageBox.Show("Error loading puzzle.");
+                    return;
+                }
+                solver = new Solver(puzzle);
+                ChangePuzzle(Path.GetFileNameWithoutExtension(d.FileName), true);
+            }
+        }
+        void SavePuzzle(object sender, EventArgs e)
+        {
+            var d = new SaveFileDialog
+            {
+                Title = "Save Sudoku Puzzle",
+                Filter = "TXT files|*.txt",
+                InitialDirectory = Path.GetFullPath(Directory.GetCurrentDirectory() + "\\..\\Puzzles")
+            };
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                solver.Puzzle.Save(d.FileName);
+                MessageBox.Show("Puzzle saved.", Text);
+            }
+        }
+        void SolvePuzzle(object sender, EventArgs e)
+        {
+            ChangeState(false, saveAsToolStripMenuItem.Enabled);
+            // Clear solver's guesses on a custom puzzle
+            if (solver.Puzzle.IsCustom)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        if (solver.Puzzle[x, y].Value != solver.Puzzle[x, y].OriginalValue)
+                        {
+                            solver.Puzzle[x, y].Set(0);
+                        }
+                    }
+                }
+            }
+            stopwatch = new Stopwatch();
+            var bw = new BackgroundWorker();
+            bw.DoWork += solver.DoWork;
+            bw.RunWorkerCompleted += SolverFinished;
+            stopwatch.Start();
+            bw.RunWorkerAsync();
+        }
+        void SolverFinished(object sender, RunWorkerCompletedEventArgs e)
+        {
+            stopwatch.Stop();
+            statusLabel.Text = string.Format("Solver finished in {0} seconds.", stopwatch.Elapsed.TotalSeconds);
+            solver.Puzzle.LogAction(string.Format("Solver {0} the puzzle", ((bool)e.Result) ? "completed" : "failed"));
+            logList.SelectedIndex = solver.Puzzle.Actions.Count - 1;
+            logList.Select();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
