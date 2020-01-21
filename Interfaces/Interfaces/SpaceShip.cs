b75429197a875be8944e844c0ca41d3dd85e5581
Joseph Robinson <smartech56@gmail.com>
2020-01-21 09:23:17 -0700

    Updated SpaceShip Sequence

--- Interfaces/Interfaces/SpaceShip.cs
+++ Interfaces/Interfaces/SpaceShip.cs
@@ -8,44 +8,105 @@ namespace Interfaces
 {
     class SpaceShip : IJoystick
     {
+        private int m_sequence;
         public bool Up()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 0 || m_sequence == 1)
+            {
+                m_sequence++;
+            }
+            else
+            {
+                m_sequence = 0;
+            }
+
+            return true;
         }
 
         public bool Down()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 2 || m_sequence == 3)
+            {
+                m_sequence++;
+            }
+            else
+            {
+                m_sequence = 0;
+            }
+
+            return true;
         }
 
         public bool Left()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 4 || m_sequence == 6)
+            {
+                m_sequence++;
+            }
+            else
+            {
+                m_sequence = 0;
+            }
+
+            return true;
         }
 
         public bool Right()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 5 || m_sequence == 7)
+            {
+                m_sequence++;
+            }
+            else
+            {
+                m_sequence = 0;
+            }
+
+            return true;
         }
 
         public bool A()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 9)
+            {
+                m_sequence++;
+            }
+            else
+            {
+                m_sequence = 0;
+            }
+
+            return true;
         }
 
         public bool B()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 8)
+            {
+                m_sequence++;
+            }
+            else
+            {
+                m_sequence = 0;
+            }
+
+            return true;
         }
 
         public bool Select()
         {
-            throw new NotImplementedException();
+            m_sequence = 0;
+            return true;
         }
 
         public bool Start()
         {
-            throw new NotImplementedException();
+            if (m_sequence == 10)
+            {
+                Console.Beep();
+            }
+            m_sequence = 0;
+            return true;
         }
     }
 }
