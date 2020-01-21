18f6a3404498cfbcf406507fabc7882fe7308e83
Joseph Robinson <smartech56@gmail.com>
2020-01-21 09:01:57 -0700

    Added Interfaces

---
+++ Interfaces/Interfaces/IJoystick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IJoystick
+    {
+        Boolean Up();
+        Boolean Down();
+        Boolean Left();
+        Boolean Right();
+        Boolean Fire();
+    }
+}
---
+++ Interfaces/Interfaces/SpaceShip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class SpaceShip : IJoystick
+    {
+        public bool Up()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Left()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Right()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Fire()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
