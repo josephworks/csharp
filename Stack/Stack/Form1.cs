64928f0f62448c6be1982a5fdaae8736a50d70b0
Joseph Robinson <smartech56@gmail.com>
2020-01-30 11:21:00 -0700

    Updated stack

---
+++ Stack/Stack/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Stack
+{
+    public partial class Form1 : Form
+    {
+        Stack s = new Stack();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Stack/Stack/IStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    interface IStack
+    {
+        void push(int n);
+        int pop();
+        int peek();
+        int size();
+    }
+}
