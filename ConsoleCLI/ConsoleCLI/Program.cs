9c0963a3f5c8a6e16293e144d79a8abd4027c7e3
Joseph Robinson <smartech56@gmail.com>
2020-02-12 09:33:11 -0700

    added consolecli

---
+++ ConsoleCLI/ConsoleCLI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+//using CmdLine;
+//using CommandLine;
+using static System.Console;
+
+namespace ConsoleCLI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string cmd = "";
+
+            WriteLine("JCLI v1.0 - Copyright JosephWorks 2020");
+
+            while (cmd != null && cmd.ToLower() != "exit")
+            {
+                Write("JCLI>");
+                cmd = ReadLine();
+                Process(cmd);
+            }
+        }
+
+        public static void Process(string cmd)
+        {
+            String args = "";
+
+            if (cmd.Contains(" "))
+            {
+                args = cmd.Substring(cmd.IndexOf(" ", StringComparison.Ordinal));
+                cmd = cmd.Substring(0, cmd.IndexOf(" ", StringComparison.Ordinal));
+            }
+
+            switch (cmd)
+            {
+                case ("beep"):
+                    Beep();
+                    break;
+
+                case ("exists"):
+                    Write("Blank Text");
+                    break;
+
+                default:
+                    WriteLine("Command not found");
+                    break;
+            }
+        }
+    }
+}
