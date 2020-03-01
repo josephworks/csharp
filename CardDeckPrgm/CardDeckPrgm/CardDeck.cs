6f03ffe7df80c2f5652c5a1cc4a2b173e150be92
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:29:44 -0700

    Added CardDeckPrgm

---
+++ CardDeckPrgm/CardDeckPrgm/CardDeck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardDeckPrgm
+{
+    class CardDeck
+    {
+        Boolean[] m_cards;
+        Random m_random = new Random();
+        public CardDeck()
+        {
+            m_cards = new Boolean[52];
+        }
+        public void Deal()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ CardDeckPrgm/CardDeckPrgm/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace CardDeckPrgm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Shuffle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CardDeckPrgm.Properties.Resources._1080a4bd1a33cec92019fab5efb3995d;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Deal";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(789, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(678, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(567, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(457, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(346, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(789, 318);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox7.TabIndex = 17;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(678, 318);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox8.TabIndex = 16;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(567, 318);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox9.TabIndex = 15;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(457, 318);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox10.TabIndex = 14;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(346, 318);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(93, 120);
+            this.pictureBox11.TabIndex = 13;
+            this.pictureBox11.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "2_of_clubs.png");
+            this.imageList1.Images.SetKeyName(1, "2_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(2, "2_of_hearts.png");
+            this.imageList1.Images.SetKeyName(3, "2_of_spades.png");
+            this.imageList1.Images.SetKeyName(4, "3_of_clubs.png");
+            this.imageList1.Images.SetKeyName(5, "3_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(6, "3_of_hearts.png");
+            this.imageList1.Images.SetKeyName(7, "3_of_spades.png");
+            this.imageList1.Images.SetKeyName(8, "4_of_clubs.png");
+            this.imageList1.Images.SetKeyName(9, "4_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(10, "4_of_hearts.png");
+            this.imageList1.Images.SetKeyName(11, "4_of_spades.png");
+            this.imageList1.Images.SetKeyName(12, "5_of_clubs.png");
+            this.imageList1.Images.SetKeyName(13, "5_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(14, "5_of_hearts.png");
+            this.imageList1.Images.SetKeyName(15, "5_of_spades.png");
+            this.imageList1.Images.SetKeyName(16, "6_of_clubs.png");
+            this.imageList1.Images.SetKeyName(17, "6_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(18, "6_of_hearts.png");
+            this.imageList1.Images.SetKeyName(19, "6_of_spades.png");
+            this.imageList1.Images.SetKeyName(20, "7_of_clubs.png");
+            this.imageList1.Images.SetKeyName(21, "7_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(22, "7_of_hearts.png");
+            this.imageList1.Images.SetKeyName(23, "7_of_spades.png");
+            this.imageList1.Images.SetKeyName(24, "8_of_clubs.png");
+            this.imageList1.Images.SetKeyName(25, "8_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(26, "8_of_hearts.png");
+            this.imageList1.Images.SetKeyName(27, "8_of_spades.png");
+            this.imageList1.Images.SetKeyName(28, "9_of_clubs.png");
+            this.imageList1.Images.SetKeyName(29, "9_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(30, "9_of_hearts.png");
+            this.imageList1.Images.SetKeyName(31, "9_of_spades.png");
+            this.imageList1.Images.SetKeyName(32, "10_of_clubs.png");
+            this.imageList1.Images.SetKeyName(33, "10_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(34, "10_of_hearts.png");
+            this.imageList1.Images.SetKeyName(35, "10_of_spades.png");
+            this.imageList1.Images.SetKeyName(36, "ace_of_clubs.png");
+            this.imageList1.Images.SetKeyName(37, "ace_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(38, "ace_of_hearts.png");
+            this.imageList1.Images.SetKeyName(39, "ace_of_spades.png");
+            this.imageList1.Images.SetKeyName(40, "jack_of_clubs.png");
+            this.imageList1.Images.SetKeyName(41, "jack_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(42, "jack_of_hearts.png");
+            this.imageList1.Images.SetKeyName(43, "jack_of_spades.png");
+            this.imageList1.Images.SetKeyName(44, "king_of_clubs.png");
+            this.imageList1.Images.SetKeyName(45, "king_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(46, "king_of_hearts.png");
+            this.imageList1.Images.SetKeyName(47, "king_of_spades.png");
+            this.imageList1.Images.SetKeyName(48, "queen_of_clubs.png");
+            this.imageList1.Images.SetKeyName(49, "queen_of_diamonds.png");
+            this.imageList1.Images.SetKeyName(50, "queen_of_hearts.png");
+            this.imageList1.Images.SetKeyName(51, "queen_of_spades.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 450);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Cards";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
