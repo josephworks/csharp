f6909db33c7fba2adeb194dfee67a366fa8d971e
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:32:05 -0700

    Added Final

---
+++ Final/Final/Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class Matrix
+    {
+        int[,] m_a = new int[1, 1];
+        public Matrix(int[,] a)
+        {
+            m_a = a;
+        }
+        public Matrix scalarMultiply(int n)
+        {
+            m_a[0, 0] *= n;
+            m_a[0, 1] *= n;
+            m_a[1, 0] *= n;
+            m_a[1, 1] *= n;
+            Matrix o = new Matrix(m_a);
+            return o;
+        }
+        public Matrix add(Matrix other)
+        {
+            m_a[0, 0] += other.m_a[0, 0];
+            m_a[0, 1] += other.m_a[0, 1];
+            m_a[1, 0] += other.m_a[1, 0];
+            m_a[1, 1] += other.m_a[1, 1];
+            Matrix o = new Matrix(m_a);
+            return o;
+        }
+        public override string ToString()
+        {
+            return "|" + m_a[0, 0] + " " + m_a[0, 1] + "|" + "\n" + "|" + m_a[1, 0] + " " + m_a[1, 1] + "|";
+        }
+    }
+}
---
+++ Final/Final/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a1 = { { 1, 1 }, { 1, 1 } };
+            int[,] a2 = { { 1, 2 }, { 3, 4 } };
+
+            Matrix m = new Matrix(a1);
+            Matrix m2 = new Matrix(a2);
+
+            WriteLine(m);
+            WriteLine(" + ");
+            WriteLine(m2);
+            WriteLine(" = ");
+            WriteLine(m.add(m2));
+            WriteLine();
+
+            WriteLine(2);
+            WriteLine(" * ");
+            WriteLine(m2);
+            WriteLine(" = ");
+            WriteLine(m2.scalarMultiply(2));
+            ReadKey();
+        }
+    }
+}
