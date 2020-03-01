e17b829c098dc1261a5097084ab44b2321e0c3f2
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:33:22 -0700

    Added MethodQuiz

---
+++ MethodQuiz/MethodQuiz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MethodQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ft;
+            int inches;
+            double m;
+            WriteLine("Enter Feet");
+            ft = int.Parse(ReadLine());
+            WriteLine("Enter Inches");
+            inches = int.Parse(ReadLine());
+            m = ftTom(ft, inches);
+            WriteLine("Meters: {0:N2}", m);
+            WriteLine("");
+
+            int l;
+            int w;
+            int h;
+            int sarea;
+            WriteLine("Enter Length");
+            l = int.Parse(ReadLine());
+            WriteLine("Enter Width");
+            w = int.Parse(ReadLine());
+            WriteLine("Enter Height");
+            h = int.Parse(ReadLine());
+            sarea = SA(l, w, h);
+            WriteLine("Surface Area: " + sarea);
+            ReadLine();
+        }
+        public static double ftTom(int feet, int inches)
+        {
+            return .0254 * ((feet * 12) + inches);
+        }
+        public static int SA(int l, int w, int h)
+        {
+            return 2 * ((w * l) + (h * l) + (h * w));
+        }
+    }
+}
