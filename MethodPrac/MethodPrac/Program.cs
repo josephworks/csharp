057a3debf5446fd1ead73b8492a9a64928c43650
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:33:07 -0700

    Added MethodPrac

---
+++ MethodPrac/MethodPrac/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MethodPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Convert(GetSeconds()));
+            ReadLine();
+        }
+        //get seconds
+        public static int GetSeconds()
+        {
+            WriteLine("Enter seconds:");
+            return int.Parse(ReadLine());
+        }
+        //convert
+        public static string Convert(int seconds)
+        {
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            int sec = seconds % 60;
+            return hours + " hours, " + minutes + " minutes, and " + sec + " seconds";
+        }
+        //output results
+    }
+}
+
