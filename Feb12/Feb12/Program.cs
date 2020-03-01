81b5b184e24cc566c36d05c7603e31d90a9a5f72
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:31:54 -0700

    Added Feb12 - MyExplorer

---
+++ Feb12/Feb12/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Feb12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cmd = "";
+            while (cmd.ToLower() != "exit")
+            {
+                Write("<");
+                cmd = ReadLine();
+                Process(cmd);
+            }
+        }
+        public static void Process(string cmd)
+        {
+            string[] args = cmd.Split(' ');
+            cmd = args[0];
+            switch (cmd.ToLower())
+            {
+                case ("beep"):
+                    Beep();
+                    break;
+                case ("exists"):
+                    WriteLine(File.Exists(args[1]));
+                    break;
+                case ("pwd"):
+                    WriteLine(Directory.GetCurrentDirectory());
+                    break;
+                case ("ls"):
+                case ("dir"):
+                    foreach (string d in Directory.GetDirectories(Directory.GetCurrentDirectory()))
+                    {
+                        WriteLine(d + "\\");
+                    }
+                    foreach (string f in Directory.GetFiles(Directory.GetCurrentDirectory()))
+                    {
+                        WriteLine(f);
+                    }
+                    break;
+                case ("cd"):
+                    if (args.Length > 0)
+                    {
+                        if (args[1] == "..")
+                        {
+                            Directory.SetCurrentDirectory(Directory.GetCurrentDirectory().ToString());
+                        }
+                    }
+                    Directory.SetCurrentDirectory(args[1]);
+                    break;
+                case ("copy"):
+                    File.Copy(args[1], args[2]);
+                    break;
+                case ("del"):
+                    File.Delete(args[1]);
+                        break;
+                case ("ren"):
+                    File.Move(args[1], args[2]);
+                    break;
+                case ("fileinfo"):
+                    FileInfo fi = new FileInfo(args[1]);
+                    WriteLine(fi.Length + " " + fi.LastWriteTime + " " + fi.CreationTime + " " + fi.IsReadOnly);
+                    break;
+                default:
+                    WriteLine("Command not found");
+                    break;
+            }
+        }
+    }
+}
---
+++ Feb12/MyExplorer/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add("a");
+            treeView1.Nodes.Add("b");
+            treeView1.Nodes.Add("c");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListViewItem i = listView1.Items.Add("a");
+            i.SubItems.Add("i");
+            i.ImageIndex = 1;
+            listView1.Items.Add("b").SubItems.Add("2");
+            listView1.Items.Add("c").SubItems.Add("3");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.SetCurrentDirectory("c:\\");
+            foreach (string d in Directory.GetDirectories("c:\\"))
+            {
+                TreeNode t = treeView1.Nodes.Add(d);
+                t.Nodes.Add("x");
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView1.Items.Clear();
+            Directory.SetCurrentDirectory(treeView1.SelectedNode.Text);
+            FileInfo fi = new FileInfo(treeView1.SelectedNode.Text);
+            foreach (string f in Directory.GetFiles(Directory.GetCurrentDirectory()))
+            {
+                ListViewItem i = listView1.Items.Add(f);
+                i.SubItems.Add(fi.CreationTime.ToString());
+                i.SubItems.Add(fi.LastWriteTime.ToString());
+            }
+        }
+    }
+}
