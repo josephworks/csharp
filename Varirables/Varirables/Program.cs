e83c69dc1e3cec4f055f36a2f9b42a38be1c2f07
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:36:02 -0700

    Added Variables

---
+++ Varirables/Varirables/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Varirables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  byte b = 1;
+            //  WriteLine("byte: " + b);
+            //  sbyte s = 2;
+            //  WriteLine("sbyte: " + s);
+            //  char c = 'c';
+            //  WriteLine("char: " + c);
+            decimal d = 92675.19m;
+            bool b = true;
+            //  WriteLine("decimal: " + d);
+            //  double dd = 9.23;
+            //  WriteLine("double: " + dd);
+            //  float f = 3;
+            //  WriteLine("float: " + f);
+            //  int i = 8;
+            //  WriteLine("int: " + i);
+            //  uint u = 5;
+            //  WriteLine("uint: " + u);
+            //  long l = 7;
+            //  WriteLine("long: " + l);
+            //  ulong ul = 10;
+            //  WriteLine("ulong: " + ul);
+            //  short sh = 9;
+            //  WriteLine("short: " + sh);
+            //  ushort us = 21;
+            //   WriteLine("ushort: " + us);
+            WriteLine("My Salary is {0:c} and it is {1} that I am full time.", d, b);
+            ReadLine();
+        }
+    }
+}
