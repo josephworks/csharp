c46ae04e23f9a38208f435755414a51ebd5d0e9e
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:30:35 -0700

    added ClassPractice

---
+++ ClassPractice/ClassPractice/Beeper.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassPractice
+{
+    internal class Beeper
+    {
+        public int m_duration;
+        public int m_pitch;
+
+        public Beeper()
+        {
+            m_duration = 500;
+            m_pitch = 1000;
+        }
+
+        public void Beep()
+        {
+            System.Console.Beep();
+        }
+
+        public void multiBeep(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                System.Threading.Thread.Sleep(100);
+                Beep();
+            }
+        }
+
+        public void superBeep(int p, int f)
+        {
+            System.Console.Beep(f, p);
+        }
+    }
+}
---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beeper b = new Beeper();
+            b.m_duration = 2000;
+            b.m_pitch = 500;
+            b.superBeep(32000, 32000);
+            ReadKey();
+        }
+    }
+}
