36456aab16858d2878cd91b7569d7c82b465c091
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:34:29 -0700

    Added Tuesday

---
+++ Tuesday/Tuesday/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Tuesday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = square(7);
+            WriteLine("The square of 7 is {0}", answer);
+            ReadLine();
+        }
+        private static int square(int num)
+        {
+            return num * num;
+        }
+    }
+}
