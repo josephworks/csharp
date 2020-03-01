6d7934c4617b2a61017b6aa9e797dab3887eb936
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:34:05 -0700

    Added Quiz

---
+++ Quiz/Quiz/CookieClicker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class CookieClicker
+    {
+        int m_cookies;
+        int m_cpc = 1;
+        public CookieClicker()
+        {
+
+        }
+        public void Click()
+        {
+            m_cookies += m_cpc;
+        }
+        public void Upgrade()
+        {
+            if (m_cookies >= 10)
+            {
+                m_cpc += m_cookies / 100;
+                m_cookies = 0;
+            }
+        }
+        public string Score()
+        {
+            return m_cookies + " Cookies";
+        }
+        public string CPC()
+        {
+            return m_cpc + " cookies per click";
+        }
+    }
+}
---
+++ Quiz/Quiz/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        CookieClicker c = new CookieClicker();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            c.Click();
+            label1.Text = c.Score();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            c.Upgrade();
+            label1.Text = c.Score();
+            label2.Text = c.CPC();
+        }
+    }
+}
