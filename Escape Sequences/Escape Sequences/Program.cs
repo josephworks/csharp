7f3b7c79e03fca15b91092ab70181958df96706e
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:31:24 -0700

    Added Escape Sequences

---
+++ Escape Sequences/Escape Sequences/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Escape_Sequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(" _____");
+            WriteLine("( * * )");
+            WriteLine("(_____)");
+            WriteLine("   |");
+            WriteLine("  /|\\");
+            WriteLine(" / | \\");
+            WriteLine("   |");
+            WriteLine("  / \\");
+            WriteLine(" /   \\");
+
+            WriteLine("");
+            WriteLine("");
+
+            WriteLine("*********" + "\t" + "     *     " + "\t" + "     ***********     ");
+            WriteLine("*********" + "\t" + "    ***    " + "\t" + "    *************    ");
+            WriteLine("*********" + "\t" + "   *****   " + "\t" + "   ***************   ");
+            WriteLine("*********" + "\t" + "  *******  " + "\t" + "  *****************  ");
+            WriteLine("*********" + "\t" + " ********* " + "\t" + " ******************* ");
+            WriteLine("*********" + "\t" + "***********" + "\t" + "*********************");
+
+            WriteLine("");
+            WriteLine("");
+
+            WriteLine("TTTTTTTTTTT" + "\t" + "     A     " + "\t" + "MM      MM");
+            WriteLine("     T     " + "\t" + "    A A    " + "\t" + "M M    M M");
+            WriteLine("     T     " + "\t" + "   A   A   " + "\t" + "M  M  M  M");
+            WriteLine("     T     " + "\t" + "  AAAAAAA  " + "\t" + "M   MM   M");
+            WriteLine("     T     " + "\t" + " A       A " + "\t" + "M        M");
+            WriteLine("     T     " + "\t" + "A         A" + "\t" + "M        M");
+
+            ReadLine();
+        }
+    }
+}
