b6bb74a9d05970149c66ed2a25dc0ca18b42eb86
Joseph Robinson <smartech56@gmail.com>
2020-02-29 20:36:44 -0700

    Added Yahtz33

---
+++ Yahtz33/Yahtzee/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class Dice
+    {
+        public int Roll()
+        {
+            Random r = new Random();
+             return r.Next(0, 6);
+        }
+        public int Value()
+        {
+            return Roll();
+        }
+    }
+}
---
+++ Yahtz33/Yahtzee/SetofDice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class SetofDice
+    {
+        Dice[] d = new Dice[5];
+        public void Roll()
+        {
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i].Roll();
+            }
+        }
+        public int score()
+        {
+            return d[1].Value() + d[2].Value() + d[3].Value() + d[4].Value() + d[5].Value();
+        }
+
+    }
+}
