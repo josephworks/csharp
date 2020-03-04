e517c9f8a9d3310ec313879e407b2c9e8c00aba4
Joseph Robinson <smartech56@gmail.com>
2020-03-04 09:14:32 -0700

    Updated Yahtz33

--- Yahtz33/Yahtzee/Dice.cs
+++ Yahtz33/Yahtzee/Dice.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Yahtzee
 {
-    class Dice
+    internal class Dice
     {
         public int Roll()
         {
             Random r = new Random();
-             return r.Next(0, 6);
+            return r.Next(0, 6);
         }
+
         public int Value()
         {
             return Roll();
--- Yahtz33/Yahtzee/SetofDice.cs
+++ Yahtz33/Yahtzee/SetofDice.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Yahtzee
+﻿namespace Yahtzee
 {
-    class SetofDice
+    internal class SetofDice
     {
-        Dice[] d = new Dice[5];
+        private Dice[] d = new Dice[5];
+
         public void Roll()
         {
             for (int i = 0; i < d.Length; i++)
@@ -16,10 +11,10 @@ namespace Yahtzee
                 d[i].Roll();
             }
         }
+
         public int score()
         {
             return d[1].Value() + d[2].Value() + d[3].Value() + d[4].Value() + d[5].Value();
         }
-
     }
 }
